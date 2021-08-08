27fde1aa791eadca4006248f0f14635cc2307258
Kaka.Go <kaka.go.work@gmail.com>
2021-08-08 15:07:57 +0900

    lesson 5 GenomicRangeQuery, prefixSum O(N+M)

---
+++ codility/lessons/5.GenomicRangeQuery O(N+M).java
@@ -0,0 +1,86 @@
+/*
+A DNA sequence can be represented as a string consisting of the letters A, C, G and T, which correspond to the types of successive nucleotides in the sequence. Each nucleotide has an impact factor, which is an integer. Nucleotides of types A, C, G and T have impact factors of 1, 2, 3 and 4, respectively. You are going to answer several queries of the form: What is the minimal impact factor of nucleotides contained in a particular part of the given DNA sequence?
+
+The DNA sequence is given as a non-empty string S = S[0]S[1]...S[N-1] consisting of N characters. There are M queries, which are given in non-empty arrays P and Q, each consisting of M integers. The K-th query (0 ≤ K < M) requires you to find the minimal impact factor of nucleotides contained in the DNA sequence between positions P[K] and Q[K] (inclusive).
+
+For example, consider string S = CAGCCTA and arrays P, Q such that:
+
+    P[0] = 2    Q[0] = 4
+    P[1] = 5    Q[1] = 5
+    P[2] = 0    Q[2] = 6
+The answers to these M = 3 queries are as follows:
+
+The part of the DNA between positions 2 and 4 contains nucleotides G and C (twice), whose impact factors are 3 and 2 respectively, so the answer is 2.
+The part between positions 5 and 5 contains a single nucleotide T, whose impact factor is 4, so the answer is 4.
+The part between positions 0 and 6 (the whole string) contains all nucleotides, in particular nucleotide A whose impact factor is 1, so the answer is 1.
+Write a function:
+
+class Solution { public int[] solution(String S, int[] P, int[] Q); }
+
+that, given a non-empty string S consisting of N characters and two non-empty arrays P and Q consisting of M integers, returns an array consisting of M integers specifying the consecutive answers to all queries.
+
+Result array should be returned as an array of integers.
+
+For example, given the string S = CAGCCTA and arrays P, Q such that:
+
+    P[0] = 2    Q[0] = 4
+    P[1] = 5    Q[1] = 5
+    P[2] = 0    Q[2] = 6
+the function should return the values [2, 4, 1], as explained above.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [1..100,000];
+M is an integer within the range [1..50,000];
+each element of arrays P, Q is an integer within the range [0..N − 1];
+P[K] ≤ Q[K], where 0 ≤ K < M;
+string S consists only of upper-case English letters A, C, G, T.
+Copyright 2009–2021 by Codility Limited. All Rights Reserved. Unauthorized copying, publication or disclosure prohibited.
+*/
+// you can also use imports, for example:
+import java.util.*;
+
+// you can write to stdout for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+
+class Solution {
+    public int[] solution(String S, int[] P, int[] Q) {
+        Map<Character, Integer> factorMap = new HashMap<Character, Integer>();
+        factorMap.put('A', 1);
+        factorMap.put('C', 2);
+        factorMap.put('G', 3);
+        factorMap.put('T', 4);
+        int[] DNA_Cum = new int[5];
+        int[][] prefixSum = new int[5][S.length()+1];
+        for(int i=1; i<S.length()+1; i++){
+            int factor = factorMap.get(S.charAt(i-1));
+            DNA_Cum[factor] ++;
+            for(int f=1; f<=4; f++){
+                prefixSum[f][i] = DNA_Cum[f];
+            }
+        }
+
+        // for(int[] sum : prefixSum){
+        //     System.out.println(Arrays.toString(sum));
+        // }
+
+        int M = P.length;
+        int[] result = new int[M];
+        for(int m=0; m<M; m++){
+            int start = P[m];
+            int end = Q[m] + 1;
+            for(int f=1; f<=4; f++){
+                int DNA_change = prefixSum[f][end] - prefixSum[f][start];
+                if(DNA_change > 0){
+                    result[m] = f;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}
+
+// O(N + M)
+// https://app.codility.com/demo/results/trainingPXMCWH-3BW/
+
